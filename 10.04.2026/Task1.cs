7670d39c7efd0244c95786b36f385a79b36b0164
AnnaKomova <akomova456@gmail.com>
2026-04-10 14:14:07 +0300

    Create Task1.cs

---
+++ 10.04.2026/Task1.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab9.Green
+{
+    internal class Task1 : Green
+    {
+        private int _output;
+
+        public int Output => _output;
+
+        public Task1(string input) : base(input)
+        {
+            _output = 0;
+        }
+        public override void Review()
+        {
+            // исходный текст лежит в input
+            // здесь решение
+            // полученный ответ записываем в поле _output
+            // Review ничего не возвращает
+            // input менять нельзя (сделать копию если надо)
+
+            throw new NotImplementedException();
+        }
+
+        public override string ToString()
+        {
+            return null;
+        }
+    }
+}
